1256c65ad7de8e8c4acb495e31831287c75563ae
Thebp <chrpe15@student.sdu.dk>
2019-03-26 09:56:32 +0100

    Added code that makes the pycom react to light level

--- IOT-Project/client/boot.py
+++ IOT-Project/client/boot.py
@@ -4,9 +4,9 @@ wlan = WLAN(mode=WLAN.STA)
 
 nets = wlan.scan()
 for net in nets:
-    if net.ssid == 'Oneplus 6':
+    if net.ssid == 'OnePlus 5':
         print('Network found!')
-        wlan.connect(net.ssid, auth=(net.sec, 'csgg1469'), timeout=5000)
+        wlan.connect(net.ssid, auth=(net.sec, 'eef2a2620ab5'), timeout=5000)
         while not wlan.isconnected():
             machine.idle() # save power while waiting
         print('WLAN connection succeeded!')
--- IOT-Project/client/main.py
+++ IOT-Project/client/main.py
@@ -1,11 +1,19 @@
 import urequests as requests
 from lib.LTR329ALS01 import LTR329ALS01
-
+import time
+import pycom
 lt = LTR329ALS01()
 
-url = 'http://192.168.43.85:5467/light'
-data = {'data':'hej marc'}
-print('Et eller  andet')
-r = requests.post(url, json=data)
-print('Nu printer vi JSON\n' + r.text)
-print(str(r.content))
+url = 'http://mndkk.dk:50001/light'
+#data = {'data':'christian'}
+#r = requests.post(url, json=data)
+#print(str(r.content))
+while True:
+    light_data = str(lt.light()[0])
+    data = {'data':light_data}
+    print(light_data)
+    r2 = requests.post(url, json=data)
+    print(r2.content)
+    print(hex(int(r2.content)))
+    pycom.rgbled(int(r2.content))
+    time.sleep(1)
