9d9dfa4b72f7794772e1201e36ce31956c23a8d4
Marc Kristensen <marcndkk@gmail.com>
2019-05-28 09:34:01 +0200

    Updated boot file for project client

--- IOT-Project/client/boot.py
+++ IOT-Project/client/boot.py
@@ -1,14 +1,27 @@
 from network import WLAN
 import machine
+import pycom
+import time
+
+pycom.hearbeat(False)
 wlan = WLAN(mode=WLAN.STA)
 
-nets = wlan.scan()
-for net in nets:
-    if net.ssid == 'OnePlus 5':
+ssid = "OnePlus 5"
+password = "eef2a2620ab5"
+
+access_points = wlan.scan()
+for ap in access_points:
+    if ap.ssid == ssid:
         print('Network found!')
-        wlan.connect(net.ssid, auth=(net.sec, 'eef2a2620ab5'), timeout=5000)
+        wlan.connect(ap.ssid, auth=(ap.sec, password))
         while not wlan.isconnected():
             machine.idle() # save power while waiting
+        
         print('WLAN connection succeeded!')
-        break
-        #machine.main('main.py')
+        # 5 second blue flash to show successful connection
+        pycom.rgbled(0x0000FF)
+        time.sleep(5)
+        pycom.rgbled(0x000000)
+
+        machine.main('main.py')
+        break   
