8c61b385b464570ea1a08848bb2140b4282ca8f8
Thebp <chrpe15@student.sdu.dk>
2019-05-28 11:31:55 +0200

    Added websocket and initial attempt at getting RGB

---
+++ IOT-Project/website/iot-app/src/app/wsadapter.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {Subject} from 'rxjs';
+import {WebsocketService} from './websocket.service'
+import {map} from 'rxjs/operators'
+
+const CHAT_URL = "ws://echo.websocket.org/";
+
+export interface Message{
+  author: string;
+  message: string;
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class WsadapterService {
+public messages: Subject<Message>;
+
+  constructor(wsService : WebsocketService) {
+    this.messages = <Subject<Message>>wsService.connect(CHAT_URL).pipe(map(
+      (response: MessageEvent): Message => {
+        let data = JSON.parse(response.data);
+        return {
+          author: data.author,
+          message: data.message
+        }
+      }
+    ))
+   }
+}
