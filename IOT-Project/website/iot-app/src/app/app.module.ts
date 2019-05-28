a8e02b303516eaa6cfb33237b4722597dcc368c4
Thebp <chrpe15@student.sdu.dk>
2019-05-28 18:53:03 +0200

    Added DragAndDrop

--- IOT-Project/website/iot-app/src/app/app.module.ts
+++ IOT-Project/website/iot-app/src/app/app.module.ts
@@ -3,18 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {ColorPickerModule} from './color-picker/color-picker.module'
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material';
+import {MaterialModule} from 'src/app/material/material.module'
+import {TreeFlatOverviewExample} from 'src/app/tree-flat-overview-example'
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+//import {DragdropComponent} from 'src/app/dragdrop/dragdrop.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule, ColorPickerModule, HttpClientModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    BrowserModule, ColorPickerModule, HttpClientModule, BrowserAnimationsModule
   ],
+  declarations: [
+    AppComponent,
+    TreeFlatOverviewExample,
+  ],
+  entryComponents: [TreeFlatOverviewExample],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, TreeFlatOverviewExample]
 })
 export class AppModule {
 
  }
+platformBrowserDynamic().bootstrapModule(AppModule);
